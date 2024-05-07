bf63771002fc023756d5ceda79fb077d712ec440
Negrie Motsetse <motsetsenegrie@gmail.com>
2024-05-07 15:11:58 +0200

    My first commit

---
+++ src/main/java/com/mycompany/negrie220757534/Negrie220757534.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.negrie220757534;
+
+/**
+ *
+ * @author Student
+ */
+public class Negrie220757534 {
+
+   public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
